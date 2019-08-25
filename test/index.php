d5a2d39d52521b72c39b4f3337a7a8c759e9a81f
lucliscio <lucliscio@h0model.org>
2019-08-25 15:00:56 +0200

    Inserting test for logger file appender

--- test/index.php
+++ test/index.php
@@ -26,17 +26,24 @@ if(version_compare(PHP_VERSION, '5.1.2', '>=')){
 } else {
     echo "ERRORE hai PHP ".PHP_VERSION."<br/>";
 }
+
+$_SESSION["script_path"] = __DIR__;
+
 echo "Lib path: ".$_SESSION["hzSystem_path"]."hzsystem".DIRECTORY_SEPARATOR."<br/>";
 echo "-----<br/>";
 echo "Locale: ".getenv("LANG")."<br/>";
-echo "Script path: ".$_SERVER["DOCUMENT_ROOT"]."<br/>";
+echo "Script path: ".$_SESSION["script_path"]."<br/>";
 echo "Language path: ".$_SESSION["hzSystem_path"]."hzsystem".DIRECTORY_SEPARATOR."lang<br/>";
-echo "Log path: ".$_SERVER["DOCUMENT_ROOT"].DIRECTORY_SEPARATOR."log<br/>";
+echo "Log path: ".$_SESSION["script_path"].DIRECTORY_SEPARATOR."log<br/>";
 echo "-----<br/>";
 echo "Start logger Test: ";
 $log = null;
 
 if($log = HZLogger::getLogger("test", HZLogger::LOG_APPENDER_FIREPHP)){
+
+    $log->add_appender(HZLogger::LOG_APPENDER_FILE);
+    $log->get_appender(HZLogger::LOG_APPENDER_FILE)->setLogDir($_SESSION["script_path"].DIRECTORY_SEPARATOR."log");
+
     $log->info("Info Test message");
     $log->error("Error Test message");
     $log->fatal("Fatal Test message");
