6e9271fdb91598c8ef363d36ecdc8318b6ef61d4
lucliscio <lucliscio@h0model.org>
2026-06-28 15:29:27 +0200

    fix: phpunit tests

--- test/tests/bootstrap.php
+++ test/tests/bootstrap.php
@@ -9,10 +9,12 @@
     // Includi il tuo autoloader personalizzato usando il percorso assoluto calcolato
     if (file_exists($baseDir . '/src/eautoloader.class.php')) {
         require_once $baseDir . '/src/eautoloader.class.php';
+        echo "Autoloader personalizzato incluso da: $baseDir/src/eautoloader.class.php\n";
     } else {
         // Se usi anche Composer come fallback/integrazione
         if (file_exists($baseDir . '/vendor/autoload.php')) {
             require_once $baseDir . '/vendor/autoload.php';
+            echo "Autoloader di Composer incluso da: $baseDir/vendor/autoload.php\n";
         } else {
             throw new RuntimeException("Impossibile trovare un autoloader valido (eautoloader.class.php o vendor/autoload.php).");
         }
